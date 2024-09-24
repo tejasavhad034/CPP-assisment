fc78d9ab7aa49b7ae72016a8988401c1d0105d30
tejasavhad034 <avhadt034@gmail.com>
2024-09-24 10:26:23 -0700

    Add files via upload

---
+++ Main (11).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// Follow the steps below to use this file
+
+// 1. In main file add the following include 
+// #include "NewFile1.cpp"
+
+// 2. Add the following in the code
+// sayHelloFromNewFile1();
+
+void sayHelloFromNewFile1() {
+    std::cout << "\nHello from New File 1!\n";
+}
