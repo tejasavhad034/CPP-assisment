2919997e57e50015ecc08070b72246c75c52d314
tejasavhad034 <avhadt034@gmail.com>
2024-09-24 11:03:46 -0700

    Add files via upload

---
+++ Main (2).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // No. of notebooks and pen
+    int notebooks = 15;
+    int pens = 20;
+    //No.of costs per notebook and per pen
+    double costPerNotebook = 2.25;
+    double costPerPen = 0.75;
+    //Adding both total costs
+
+    double totalCost = (notebooks * costPerNotebook) + (pens * costPerPen);
+
+    cout << "Total cost of the supplies: Rs. " << totalCost << endl;
+
+    return 0;
+}
